ae4eacbd81376bf7bac346d15e8688c7a83d8bb6
anoushameh <42008584+anoushameh@users.noreply.github.com>
2018-08-01 14:29:34 -0400

    My css style sheet, javascript code, and downloaded fonts that are applied to my blog!

---
+++ stuffForBlog.js
@@ -0,0 +1,5 @@
+window.alert("IF YOU LOVE ME PRESS OK! <3")
+
+function buttonFunc() {
+  document.getElementById('contactInfo').innerHTML='click the links below & my social media/accounts are listed: instagram:__anousha__//listen to my playlists:spotify:anousha mehta';
+}
